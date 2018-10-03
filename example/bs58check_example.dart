80622a040a9054e31cf6e60e5ccc7f6accb17bf5
Doan Huu An <andoan@wakumo.vn>
2018-10-03 16:42:04 +0700

    random character

--- example/bs58check_example.dart
+++ example/bs58check_example.dart
@@ -1,10 +1,9 @@
 import 'package:bs58check/bs58check.dart' as bs58check;
 import 'package:hex/hex.dart';
 main() {
-//  var payload = HEX.decode('006b4689b16668e8ba5c29c208660b36201846b2ff');
-//  var address = bs58check.encode(payload);
-//  print(address);
-//  print(HEX.encode(bs58check.decode('1AnDoanZQXAZUgFiqJ2i7Z2DPU2J6hW62i')));
-//  print(bs58check.getAddress('1AnDoanZQXAZUgFiqJ2i7Z2'));
-  print(bs58check.getAddress('ThuXemLaDaiDuocToiBaoNhieuZ'));
+  var payload = HEX.decode('006b4689b16668e8ba5c29c208660b36201846b2ff');
+  var address = bs58check.encode(payload);
+  print(address);
+  print(HEX.encode(bs58check.decode(address)));
+  print(bs58check.getAddress('AnDoan'));
 }
--- lib/src/bs58check_base.dart
+++ lib/src/bs58check_base.dart
@@ -1,5 +1,6 @@
 import 'package:crypto/crypto.dart' show sha256, Digest;
 import 'dart:typed_data';
+import 'dart:math';
 import 'utils/base.dart';
 final String ALPHABET = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz';
 final base58 = new Base(ALPHABET);
@@ -32,7 +33,11 @@ String getAddress(String inputString) {
   if (inputString.length > 27) {
     throw new ArgumentError("Can't create");
   }
-  String string = '1' + inputString.padRight(33, 'i');
+  String string = '1' + inputString;
+  for(var i = inputString.length; i < 34; i++) {
+    final rng = Random.secure();
+    string += (ALPHABET)[rng.nextInt(58)];
+  }
   Uint8List buffer = base58.decode(string);
   Uint8List payload = buffer.sublist(0, buffer.length - 4);
   String address = encode(payload);
